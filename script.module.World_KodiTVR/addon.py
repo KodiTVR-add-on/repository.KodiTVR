fd2789bc0b517982f4dff16834a320b2543d2e8c
KodiTVR-Add-on <koditvr@gmail.com>
2020-12-26 19:07:37 +0000

    UPGRADE
    UPGRADE

--- script.module.World_KodiTVR/addon.py
+++ script.module.World_KodiTVR/addon.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2016,2017,2018 RACC
+# Copyright (C) 2015 - 2020 RACC
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -9,75 +9,71 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  Go to the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+# along with this program.  If not, go to <http://www.gnu.org/licenses/>.
+#
+# RnJhbmsgdGhlIHRoaWVm
 from __future__ import unicode_literals
 
 import sys
-import xbmc
-import xbmcgui
-import xbmcaddon
-import xbmcplugin
 from xbmcgui import ListItem
+from kodi_six import xbmc, xbmcgui, xbmcaddon, xbmcplugin
 from routing import Plugin
 
 import os
-import traceback
-import requests
-import requests_cache
-from datetime import timedelta
-from base64 import b64decode, urlsafe_b64encode
-from pyDes import des, PAD_PKCS5
+import time
+from requests.exceptions import RequestException
+from resources.lib.uktvnow import UKTVNow
 
 try:
-    from urllib.parse import quote_from_bytes as orig_quote
+    from xbmcvfs import translatePath
 except ImportError:
-    from urllib import quote as orig_quote
+    from kodi_six.xbmc import translatePath
 
 addon = xbmcaddon.Addon()
 plugin = Plugin()
 plugin.name = addon.getAddonInfo("name")
-user_agent = "Dalvik/2.1.0 (Linux; U; Android 5.1.1; AFTS Build/LVY48F)"
-player_user_agent = "mediaPlayerhttp/2.4 (Linux;Android 5.1) ExoPlayerLib/2.6.1"
-USER_DATA_DIR = xbmc.translatePath(addon.getAddonInfo("profile")).decode("utf-8")  # !!
-CACHE_TIME = int(addon.getSetting("cache_time"))
-CACHE_FILE = os.path.join(USER_DATA_DIR, "cache")
-expire_after = timedelta(hours=CACHE_TIME)
-
+USER_DATA_DIR = translatePath(addon.getAddonInfo("profile"))
+data_time = int(addon.getSetting("data_time") or "0")
+cache_time = int(addon.getSetting("cache_time") or "0")
 if not os.path.exists(USER_DATA_DIR):
     os.makedirs(USER_DATA_DIR)
 
-s = requests_cache.CachedSession(CACHE_FILE, allowable_methods="POST", expire_after=expire_after, old_data_on_error=True)
-s.hooks = {"response": lambda r, *args, **kwargs: r.raise_for_status()}
-s.headers.update({"User-Agent": "USER-AGENT-tvtap-APP-V2"})
-token_url = "http://tvtap.net/tvtap1/index_tvtappro.php?case=get_channel_link_with_token_tvtap_updated"
-list_url = "http://tvtap.net/tvtap1/index_tvtappro.php?case=get_all_channels"
+
+def log(msg, level=xbmc.LOGDEBUG):
+    xbmc.log("[{0}] {1}".format(plugin.name, msg), level=level)
 
 
-def quote(s, safe=""):
-    return orig_quote(s.encode("utf-8"), safe.encode("utf-8"))
+TV = UKTVNow(USER_DATA_DIR)
+current_time = int(time.time())
+if current_time - data_time > cache_time * 60 * 60:
+    try:
+        TV.update_channels()
+        addon.setSetting("data_time", str(current_time))
+        log("[{0}] Channels updated".format(current_time))
+    except (ValueError, RequestException) as e:
+        if data_time == 0:
+            """ No data """
+            log(e.message)
+            dialog = xbmcgui.Dialog()
+            dialog.notification(plugin.name, repr(e.message), xbmcgui.NOTIFICATION_ERROR)
+            xbmcplugin.endOfDirectory(plugin.handle, False)
+        else:
+            """ Data update failed """
+            log("[{0}] Channels update fail, data age: {1}".format(current_time, data_time))
+            log(e.message)
 
 
 @plugin.route("/")
 def root():
     list_items = []
-    r = s.post(list_url, headers={"app-token": "9120163167c05aed85f30bf88495bd89"}, data={"username": "603803577"}, timeout=15)
-    if "Could not connect" in r.content:
-        s.cache.clear()
-    ch = r.json()
-    category_list = []
-    for c in ch["msg"]["channels"]:
-        category = '{0}'.format(c.get('cat_name'))
-        if category not in category_list:
-            category_list.append(category)
-            li = ListItem(category)
-            url = plugin.url_for(list_channels, cat_id=c.get('cat_id'))
-            list_items.append((url, li, True))
-
+    for cat in TV.get_categories():
+        li = ListItem(cat.cat_name, offscreen=True)
+        url = plugin.url_for(list_channels, cat_id=cat.cat_id)
+        list_items.append((url, li, True))
     xbmcplugin.addSortMethod(plugin.handle, xbmcplugin.SORT_METHOD_LABEL)
     xbmcplugin.addDirectoryItems(plugin.handle, list_items)
     xbmcplugin.endOfDirectory(plugin.handle)
@@ -86,116 +82,58 @@ def root():
 @plugin.route("/list_channels/<cat_id>")
 def list_channels(cat_id=None):
     list_items = []
-    r = s.post(list_url, headers={"app-token": "9120163167c05aed85f30bf88495bd89"}, data={"username": "603803577"}, timeout=15)
-    if "Could not connect" in r.content:
-        s.cache.clear()
-    ch = r.json()
-
-    for c in ch["msg"]["channels"]:
-        if c["cat_id"] == cat_id:
-            image = "http://tvtap.net/tvtap1/{0}|User-Agent={1}".format(quote(c.get("img"), "/"), quote(user_agent))
-            title = '{0} - {1}'.format(c.get('country'),c.get('channel_name').rstrip('.,-'))
-            li = ListItem(title)
-            li.setProperty("IsPlayable", "true")
-            li.setArt({"thumb": image, "icon": image})
-            li.setInfo(type="Video", infoLabels={"Title": title, "mediatype": "video"})
-            try:
-                li.setContentLookup(False)
-            except AttributeError:
-                pass
-            url = plugin.url_for(play, ch_id=c["pk_id"])
-            list_items.append((url, li, False))
-
+    for channel in TV.get_channels_by_category(cat_id):
+        title = "{0} - {1}".format(channel.country, channel.channel_name.rstrip(".,-"))
+        image = TV.image_url(channel.img)
+        li = ListItem(title, offscreen=True)
+        li.setProperty("IsPlayable", "true")
+        li.setArt({"thumb": image, "icon": image})
+        li.setInfo(type="Video", infoLabels={"Title": title, "mediatype": "video"})
+        url = plugin.url_for(play, pk_id=channel.pk_id)
+        list_items.append((url, li, False))
     xbmcplugin.addSortMethod(plugin.handle, xbmcplugin.SORT_METHOD_LABEL)
     xbmcplugin.addDirectoryItems(plugin.handle, list_items)
+    xbmcplugin.setContent(plugin.handle, "videos")
     xbmcplugin.endOfDirectory(plugin.handle)
 
 
-@plugin.route("/play/<ch_id>/play.pvr")
-def play(ch_id):
-    # 178.132.6.54 81.171.8.162
-    key = b"98221122"
-
-    r = s.post(list_url, headers={"app-token": "9120163167c05aed85f30bf88495bd89"}, data={"username": "603803577"}, timeout=15)
-    ch = r.json()
-    for c in ch["msg"]["channels"]:
-        if c["pk_id"] == ch_id:
-            selected_channel = c
-            break
-
-    title = selected_channel.get("channel_name")
-    image = "http://tvtap.net/tvtap1/{0}|User-Agent={1}".format(quote(c.get("img"), "/"), quote(user_agent))
-
-    with s.cache_disabled():
-        r = s.post(token_url, headers={"app-token": "9120163167c05aed85f30bf88495bd89"}, data={"channel_id": ch_id, "username": "603803577"}, timeout=15)
-
-    links = []
-    for stream in r.json()["msg"]["channel"][0].keys():
-        if "stream" in stream or "chrome_cast" in stream:
-            d = des(key)
-            link = d.decrypt(b64decode(r.json()["msg"]["channel"][0][stream]), padmode=PAD_PKCS5)
-            if link:
-                link = link.decode("utf-8")
-                if not link == "dummytext" and link not in links:
-                    links.append(link)
-
-    if addon.getSetting("autoplay") == "true":
-        link = links[0]
-    else:
-        dialog = xbmcgui.Dialog()
-        ret = dialog.select("Choose Stream", links)
-        link = links[ret]
-
-    if link.startswith("http"):
-        media_url = "{0}|User-Agent={1}".format(link, quote(player_user_agent))
-    else:
-        media_url = link
-
-    if "playlist.m3u8" in media_url:
-        if addon.getSetting("inputstream") == "true":
-            li = ListItem(title, path=media_url)
-            li.setArt({"thumb": image, "icon": image})
-            li.setMimeType("application/vnd.apple.mpegurl")
-            li.setProperty("inputstreamaddon", "inputstream.adaptive")
-            li.setProperty("inputstream.adaptive.manifest_type", "hls")
-            li.setProperty("inputstream.adaptive.stream_headers", media_url.split("|")[-1])
-        elif addon.getSetting("livestreamer") == "true":
-            serverPath = os.path.join(xbmc.translatePath(addon.getAddonInfo("path")), "livestreamerXBMCLocalProxy.py")
-            runs = 0
-            while not runs > 10:
-                try:
-                    requests.get("http://127.0.0.1:19001/version")
-                    break
-                except Exception:
-                    xbmc.executebuiltin("RunScript(" + serverPath + ")")
-                    runs += 1
-                    xbmc.sleep(600)
-            livestreamer_url = "http://127.0.0.1:19001/livestreamer/" + urlsafe_b64encode("hlsvariant://" + media_url)
-            li = ListItem(title, path=livestreamer_url)
-            li.setArt({"thumb": image, "icon": image})
-            li.setMimeType("video/x-mpegts")
-        else:
-            li = ListItem(title, path=media_url)
-            li.setArt({"thumb": image, "icon": image})
-            li.setMimeType("application/vnd.apple.mpegurl")
-    else:
-        li = ListItem(title, path=media_url)
-        li.setArt({"thumb": image, "icon": image})
-
+@plugin.route("/play/<pk_id>/play.pvr")
+def play(pk_id):
+    channel = TV.get_channel_by_id(pk_id)
+    title = "{0} - {1}".format(channel.country, channel.channel_name.rstrip(".,-"))
+    image = TV.image_url(channel.img)
     try:
-        li.setContentLookup(False)
-    except AttributeError:
-        pass
+        links = TV.get_channel_links(pk_id)
+        if addon.getSetting("autoplay") == "true":
+            link = links[0]
+        elif len(links) > 1:
+            dialog = xbmcgui.Dialog()
+            ret = dialog.select("Choose Stream", links)
+            link = links[ret]
+        else:
+            link = links[0]
 
-    xbmcplugin.setResolvedUrl(plugin.handle, True, li)
+        li = ListItem(title, path=link)
+        li.setArt({"thumb": image, "icon": image})
+        if "playlist.m3u8" in link:
+            li.setContentLookup(False)
+            li.setMimeType("application/vnd.apple.mpegurl")
+            if addon.getSetting("inputstream") == "true":
+                if sys.version_info[0] == 2:
+                    li.setProperty("inputstreamaddon", "inputstream.adaptive")
+                else:
+                    li.setProperty("inputstream", "inputstream.adaptive")
+                li.setProperty("inputstream.adaptive.manifest_type", "hls")
+                li.setProperty("inputstream.adaptive.stream_headers", link.split("|")[-1])
+
+        xbmcplugin.setResolvedUrl(plugin.handle, True, li)
+    except (ValueError, RequestException) as e:
+        log(e.message)
+        dialog = xbmcgui.Dialog()
+        dialog.notification(plugin.name, repr(e.message), xbmcgui.NOTIFICATION_ERROR)
+        xbmcplugin.setResolvedUrl(plugin.handle, False, ListItem())
 
 
 if __name__ == "__main__":
-    try:
-        plugin.run(sys.argv)
-        s.close()
-    except requests.exceptions.RequestException as e:
-        dialog = xbmcgui.Dialog()
-        dialog.notification(plugin.name, str(e), xbmcgui.NOTIFICATION_ERROR)
-        traceback.print_exc()
-        xbmcplugin.endOfDirectory(plugin.handle, False)
+    plugin.run(sys.argv)
+    del TV
